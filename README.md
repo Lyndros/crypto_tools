d16470c1c9adc034da258402dd346604be715062
Lyndros <lyndros@hotmail.com>
2018-08-08 18:13:35 +0200

    Pushing latest trading bot

--- README.md
+++ README.md
@@ -4,8 +4,10 @@ Lyndros's Crypto/Tools
 A beautiful repository for my crypto related scripts/tools.
 
 \
-|--> discord_bots/	# Bots for discord app connecting to the MN explorer API. 
-|--> services/		# Linux scripts to add MN and BOT services at boot time.
+|--> discord_bots/		# Bots for discord app connecting to the MN explorer API. 
+|--> services/		    	# Linux scripts to add MN and BOT services at boot time.
+|--> enigma_catalyst_bots/  	# Bots for different exchanges based in enigma catalyst.
+|--> binance_bots/ 		# Bots based in python binance api.
 
 Blame to: lyndros@hotmail.com
 
--- binance_bots/trade_monitor.py
+++ binance_bots/trade_monitor.py
@@ -14,20 +14,20 @@ MONITORED_COINS = (
     {"COIN_NAME":              "Storm",
      "COIN_ACRONYM":           "STORM",
      "BUY_PRICE_BTC":          "0.00000279",
-     "TAKE_PROFIT_PERCENTAGE": 15,
-     "STOP_LOSS_PERCENTAGE":   30 },
+     "TAKE_PROFIT_PERCENTAGE": +30,
+     "STOP_LOSS_PERCENTAGE":   -15 },
     #Parameters for Groestlcoin
     {"COIN_NAME":              "Groestlcoin",
      "COIN_ACRONYM":           "GRS",
      "BUY_PRICE_BTC":          "0.00008677",
-     "TAKE_PROFIT_PERCENTAGE": 15,
-     "STOP_LOSS_PERCENTAGE":   30 },
+     "TAKE_PROFIT_PERCENTAGE": +30,
+     "STOP_LOSS_PERCENTAGE":   -15 },
      #Parameters for Wanchain
      {"COIN_NAME":              "Wanchain",
       "COIN_ACRONYM":           "WAN",
       "BUY_PRICE_BTC":          "0.00034490",
-      "TAKE_PROFIT_PERCENTAGE": 15,
-      "STOP_LOSS_PERCENTAGE":   30 })
+      "TAKE_PROFIT_PERCENTAGE": +30,
+      "STOP_LOSS_PERCENTAGE":   -15 })
 
 #BINANCE API KEY TO RUN MY BOT
 API_KEY         = ""
@@ -40,9 +40,17 @@ def get_current_price_btc(coin_acronym):
     prices = client.get_all_tickers()
     #Iterate over all coins
     for myprice in prices:
-        #print(myprice)
         if myprice["symbol"] == coin_acronym+'BTC':
             return myprice["price"]
+
+def stop_loss_btc(coin_acronym):
+    print("SELL: Houston, we have a problem")
+
+def sell_coin_btc(coin_acronym):
+    print("SELL: Take the money and run")
+
+def hodl_coin_btc(coin_acronym):
+    print("HODL: Keep calm and keep holding")
 
 client = Client(API_KEY, API_SECRET)
 
@@ -67,10 +75,24 @@ for mycoin in MONITORED_COINS:
     #Display coin information
     print("######################################")
     print("Name:          %s (%s)" %(mycoin['COIN_NAME'], mycoin['COIN_ACRONYM']))
+    mycoin_current_balance = client.get_asset_balance(asset=mycoin['COIN_ACRONYM'])
+    print("Balance:       %s %s"   %(mycoin_current_balance['free'], mycoin['COIN_ACRONYM']))
     print("Buy price:     %s BTC"  %mycoin['BUY_PRICE_BTC'])
     mycoin_current_price = get_current_price_btc(mycoin['COIN_ACRONYM'])
     print("Current price: %s BTC"  %mycoin_current_price)
-    mycoin_current_balance = client.get_asset_balance(asset=mycoin['COIN_ACRONYM'])
-    print("Balance:       %s %s"   %(mycoin_current_balance['free'], mycoin['COIN_ACRONYM']))
     mycoin_delta= (float(mycoin_current_price)*100/float(mycoin['BUY_PRICE_BTC']))-100
     print("Delta:         {:.2f}%".format(mycoin_delta))
+
+    print("Stop Loss:     {0}%".format(mycoin['STOP_LOSS_PERCENTAGE']))
+    print("Take Profit:   {0}%".format(mycoin['TAKE_PROFIT_PERCENTAGE']))
+
+    #SET STOP LOSS
+    if   mycoin_delta < float(mycoin['STOP_LOSS_PERCENTAGE']):
+        stop_loss_btc(mycoin['COIN_ACRONYM'])
+    #TAKE PROFIT
+    elif float(mycoin_delta) >= float(mycoin['TAKE_PROFIT_PERCENTAGE']):
+        sell_coin_btc(mycoin['COIN_ACRONYM'])
+    #HOLD COIN
+    else:
+        hodl_coin_btc(mycoin['COIN_ACRONYM'])
+    print("######################################")
